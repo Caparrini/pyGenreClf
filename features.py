53d76c94370254736e2b5b70c7d30beadde9c566
Caparrini <Caparrini@users.noreply.github.com>
2017-04-30 01:37:40 +0200

    Fixed extracted features and added a new one for bigger dataset (kurt and skew).

--- features.py
+++ features.py
@@ -6,6 +6,7 @@ import os
 from pyAudioAnalysis import audioFeatureExtraction
 from pyAudioAnalysis import audioBasicIO
 import sys
+import time
 
 
 def create_fft(fn):
@@ -90,7 +91,7 @@ def query_yes_no(question, default="yes"):
             sys.stdout.write("Please respond with 'yes' or 'no' "
                              "(or 'y' or 'n').\n")
 
-def pyAudioFeatures(dataset_csv="beatsdataset.csv", dataset_folder="/Users/Capa/Datasets/beatsdataset"):
+def pyAudioFeatures70(dataset_csv="beatsdataset.csv", dataset_folder="/Users/Capa/Datasets/beatsdataset"):
 
     genre_list = [x for x in os.walk(dataset_folder)][0][1]
 
@@ -108,8 +109,12 @@ def pyAudioFeatures(dataset_csv="beatsdataset.csv", dataset_folder="/Users/Capa/
 
     features = []
     labels = []
-    [features.append(featuresTotal[i][ii]) for i in range(len(genre_list)) for ii in range(100)]
-    [labels.append(labelsTotal[i/100]) for i in range(len(genre_list)*100)]
+
+    for i in range(len(genre_list)):
+        for ii in range(len(featuresTotal[i])):
+            features.append(featuresTotal[i][ii])
+            labels.append(labelsTotal[i])
+
     df = pd.DataFrame.from_records(features)
 
     features_names = ["ZCR", "Energy", "EnergyEntropy", "SpectralCentroid",
@@ -129,7 +134,7 @@ def pyAudioFeatures(dataset_csv="beatsdataset.csv", dataset_folder="/Users/Capa/
             features_names_full.append(str(i + offset) + "-" + features_names[9] + str(i - 21) + features_metrics[j])
         features_names_full.append(str(34 + offset) + "-" + features_names[10] + features_metrics[j])
     features_names_full.append("69-BPM")
-    features_names_full.append("70-BPMpaa")
+    features_names_full.append("70-BPMconf")
 
     df.columns = features_names_full
 
@@ -137,3 +142,73 @@ def pyAudioFeatures(dataset_csv="beatsdataset.csv", dataset_folder="/Users/Capa/
     pd.DataFrame.to_csv(df,dataset_csv)
 
     return df
+
+def pyAudioFeatures(dataset_csv="beatsdataset.csv", dataset_folder="/Users/Capa/Datasets/beatsdataset"):
+
+    genre_list = [x for x in os.walk(dataset_folder)][0][1]
+
+    if(not os.path.exists(dataset_folder)):
+        print("The dataset folder : " + dataset_folder + " does not exist.\n")
+        return
+
+    if(os.path.exists(dataset_csv)):
+
+        print("The dataset_csv file already exists.\n")
+        if(not query_yes_no("Do you want to overwrite it?")):
+            return
+
+    dirs = [dataset_folder + f + "/" for f in genre_list]
+    [featuresTotal, labelsTotal, _] = audioFeatureExtraction.dirsWavFeatureExtraction(dirs, 1, 1, 0.05, 0.025, True)
+
+    features = []
+    labels = []
+
+    for i in range(len(genre_list)):
+        for ii in range(len(featuresTotal[i])):
+            features.append(featuresTotal[i][ii])
+            labels.append(labelsTotal[i])
+
+    df = pd.DataFrame.from_records(features)
+
+    features_names = ["ZCR", "Energy", "EnergyEntropy", "SpectralCentroid",
+                          "SpectralSpread", "SpectralEntropy", "SpectralFlux",
+                          "SpectralRolloff", "MFCCs", "ChromaVector", "ChromaDeviation"]
+
+    features_names_full = []
+    features_metrics = ["m", "std", "skew", "kurt"]
+
+    for j in range(len(features_metrics)):
+        offset = j * 34
+        for i in range(1, 9):
+            features_names_full.append(str(i + offset) + "-" + features_names[i - 1] + features_metrics[j])
+        for i in range(9, 22):
+            features_names_full.append(str(i + offset) + "-" + features_names[8] + str(i - 8) + features_metrics[j])
+        for i in range(22, 34):
+            features_names_full.append(str(i + offset) + "-" + features_names[9] + str(i - 21) + features_metrics[j])
+        features_names_full.append(str(34 + offset) + "-" + features_names[10] + features_metrics[j])
+    features_names_full.append(str(len(features_metrics)*34+1)+"-BPM")
+    features_names_full.append(str(len(features_metrics)*34+2)+"-BPMconf")
+
+    df.columns = features_names_full
+
+    df["class"] = labels
+    pd.DataFrame.to_csv(df,dataset_csv)
+
+    return df
+
+# Extract features from one single audio with x = samples and Fs = Frequency rate
+def extractFeatures(Fs, x, mtWin, mtStep, stWin, stStep):
+    t1 = time.clock()
+
+    [MidTermFeatures, stFeatures] = audioFeatureExtraction.mtFeatureExtraction(x, Fs, round(mtWin*Fs), round(mtStep*Fs), round(Fs*stWin), round(Fs * stStep))
+    [beat, beatConf] = audioFeatureExtraction.beatExtraction(stFeatures, stStep)
+
+    MidTermFeatures = np.transpose(MidTermFeatures)
+    MidTermFeatures = MidTermFeatures.mean(axis=0) # long term averaging of mid-term statistics
+
+    MidTermFeatures = np.append(MidTermFeatures, beat)
+    MidTermFeatures = np.append(MidTermFeatures, beatConf)
+
+    t2 = time.clock()
+    print("Processing time : " + str(t2-t1))
+    return MidTermFeatures
