afc9317f8824b8f68f0632c845867cd77ae92e03
Caparrini <Caparrini@users.noreply.github.com>
2017-04-29 17:48:15 +0200

    A few changes.

--- optimize.py
+++ optimize.py
@@ -1,28 +1,29 @@
 import numpy as np
 import pandas as pd
 from classifier import getClf, KFoldAccuracy
+
+
 def autoTPOT(df):
-    labels = list(df["class"].values)
+    #labels = list(df["class"].values)
     features = []
     for j in range(df.shape[0]):
         item = df.ix[j]
         features.append([item[i] for i in range(len(item) - 1)])
 
     features = np.array(features)
-    labels = np.array(labels)
 
-    labelsd, uniques = pd.factorize(df["class"])
+    labels, uniques = pd.factorize(df["class"])
 
-    from sklearn.model_selection import train_test_split
+    #from sklearn.model_selection import train_test_split
 
-    features_train, features_test, labels_train, labels_test = train_test_split(features, labelsd, test_size=0.2)
+    #features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.2)
 
     from tpot import TPOTClassifier
     pipeline_optimizer = TPOTClassifier(generations=5, population_size=3, verbosity=3)
 
-    pipeline_optimizer.fit(features_train, labels_train)
-    print(pipeline_optimizer.score(features_test, labels_test))
-    pipeline_optimizer.export('tpot_exported_pipeline3.py')
+    pipeline_optimizer.fit(features, labels)
+    #print(pipeline_optimizer.score(features_test, labels_test))
+    pipeline_optimizer.export('tpot_exported_pipeline.py')
 
 
 
