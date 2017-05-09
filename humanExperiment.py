51dcdc0d115c3b1a88050baaaf0db880f0f6012c
Caparrini <Caparrini@users.noreply.github.com>
2017-05-09 20:26:44 +0200

    Added style endline.

--- humanExperiment.py
+++ humanExperiment.py
@@ -5,12 +5,12 @@ from shutil import copy2
 from sklearn.metrics import confusion_matrix
 
 
-
 class ExperimentUtils(object):
     """
     This class is used to create experiments splitting the dataset for human classification.
     Also evaluate the performance of that classification.
     """
+
     def __init__(self, genres_folder, exp_folder):
         """
         Initialize the class
@@ -39,7 +39,7 @@ class ExperimentUtils(object):
             dftrain = pd.concat([dftrain, df2])
         anonymized_names = ["song" + str((x % 100) / 10) + str(x % 10) for x in range(1, dftest.shape[0] + 1)]
         dftest = dftest.sample(frac=1)  # Relocation at random, avoiding songs grouped by genre
-        dftest["test_name"] = anonymized_names # Use the anonymized_names with the random DataFrame
+        dftest["test_name"] = anonymized_names  # Use the anonymized_names with the random DataFrame
         return dftest, dftrain
 
     def _generate_alias_dataframes(self, genre_folder, n_songs):
@@ -58,14 +58,14 @@ class ExperimentUtils(object):
         label_names = [genre + str(x / 100) + str((x % 100) / 10) + str(x % 10) for x in range(1, len(songs) + 1)]
         labels = [genre] * 100
         df = pd.DataFrame()
-        df["class"] = labels    # genre label
-        df["real_name"] = songs # filename
+        df["class"] = labels  # genre label
+        df["real_name"] = songs  # filename
         df["label_name"] = label_names  # dataset name
         dftest, dftrain = np.split(df.sample(n_songs), 2, axis=0)
-        train_names = [self.genres_dict[genre] +    # Names according to the label in genres_dict
+        train_names = [self.genres_dict[genre] +  # Names according to the label in genres_dict
                        str((x % 100) / 10) + str(x % 10) for x in range(1, dftrain.shape[0] + 1)]
 
-        dftrain["train_name"] = train_names # Anonymizing train
+        dftrain["train_name"] = train_names  # Anonymizing train
         return dftest, dftrain
 
     def build_experiment(self, number_of_songs):
@@ -74,8 +74,8 @@ class ExperimentUtils(object):
         :param number_of_songs: Number of songs of each genre (half to train, half to test)
         :return: Nothing
         """
-        dftest, dftrain = self._generate_full_alias_dataframes(n_songs=number_of_songs) # Get names DataFrames
-        os.mkdir(self.exp_folder)   # Create main exp folder
+        dftest, dftrain = self._generate_full_alias_dataframes(n_songs=number_of_songs)  # Get names DataFrames
+        os.mkdir(self.exp_folder)  # Create main exp folder
         train_folder = os.path.join(self.exp_folder, "train")
         test_folder = os.path.join(self.exp_folder, "test")
         eval_folder = os.path.join(self.exp_folder, "evaluation")
@@ -83,23 +83,23 @@ class ExperimentUtils(object):
         os.mkdir(test_folder)
         # Create one folder for each genre in train and test folders
         for key in self.genres_dict.keys():
-            gen = dftrain["class"] == key   # Rule to select the actual genre later in dftrain
+            gen = dftrain["class"] == key  # Rule to select the actual genre later in dftrain
             os.mkdir(os.path.join(train_folder, self.genres_dict[key]))
             os.mkdir(os.path.join(test_folder, self.genres_dict[key]))  # Empty folder
             for index, row in dftrain[gen].iterrows():
-                source = os.path.join(self.genres_folder, key, row["real_name"])    # Song in the dataset folder
+                source = os.path.join(self.genres_folder, key, row["real_name"])  # Song in the dataset folder
                 target = os.path.join(train_folder,
                                       self.genres_dict[key], row["train_name"] + ".mp3")  # Song anonymized
-                copy2(source, target)   # Copying into train/label folder
+                copy2(source, target)  # Copying into train/label folder
                 print(source, target)
         # Add the test songs to the test folder
         for index, row in dftest.iterrows():
-            source = os.path.join(self.genres_folder, row["class"], row["real_name"])   # Song in the dataset folder
-            target = os.path.join(test_folder, row["test_name"] + ".mp3")   # Song anonymized
-            copy2(source, target) # Copying into test folder
+            source = os.path.join(self.genres_folder, row["class"], row["real_name"])  # Song in the dataset folder
+            target = os.path.join(test_folder, row["test_name"] + ".mp3")  # Song anonymized
+            copy2(source, target)  # Copying into test folder
             print(source, target)
 
-        os.mkdir(eval_folder)   # Create the evaluation folder to save results
+        os.mkdir(eval_folder)  # Create the evaluation folder to save results
         # Save the alias files to evaluate when the experiments will be done
         pd.DataFrame.to_csv(dftrain, os.path.join(eval_folder, "train_songs.csv"))
         pd.DataFrame.to_csv(dftest, os.path.join(eval_folder, "test_songs.csv"))
@@ -115,16 +115,17 @@ class ExperimentUtils(object):
         # Generate DataFrame with the name of the songs and the label predicted by the subject
         for key in self.inv_genres_dict:
             dfaux = pd.DataFrame()
-            songs = [y.replace(".mp3","") for y in [x for x in os.walk(os.path.join(self.exp_folder, "test", key))][0][2] if ".mp3" in y]
+            songs = [y.replace(".mp3", "") for y in
+                     [x for x in os.walk(os.path.join(self.exp_folder, "test", key))][0][2] if ".mp3" in y]
             dfaux["test_name"] = songs
             pred_labels = [self.inv_genres_dict[key]] * len(songs)
             dfaux["pred_class"] = pred_labels
             dfeval = pd.concat([dfeval, dfaux])
         # Joining the DataFrame we used and the one results from the user by the "test_name" column in both
         dfeval = pd.merge(dftest, dfeval)
-        labels = dfeval["class"]    # Real class
+        labels = dfeval["class"]  # Real class
         pred_labels = dfeval["pred_class"]  # Predicted class
         cm = confusion_matrix(labels, pred_labels, self.genres_dict.keys())
         print(cm)
-        #TODO to improve show results
-        #TODO option for save more than one result
+        # TODO to improve show results
+        # TODO option for save more than one result
