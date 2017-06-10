3a34235a424e3ece5801db4b0a6b3841d12c6388
Caparrini <Caparrini@users.noreply.github.com>
2017-06-10 20:05:41 +0200

    MODIFIED: Main

--- main.py
+++ main.py
@@ -3,24 +3,33 @@ import classifier
 import librosa
 import argparse
 import joblib
-from featuresExtraction import extractFeaturesFolder
+from featuresExtraction import extractFeaturesFolder, extractFeatures
 from optimize import ForestOptimizer, TreeOptimizer
 from classifier import KFoldCrossValidation, TreeKFoldReport
 
 def main():
-# display some lines
-    df = pd.DataFrame.from_csv("CSV/beatsdataset.csv")
-    clf = classifier.bestClfs()[1]
-    classes, features, labels = classifier.unpackDF(df)
-    clf.fit(features, labels)
+    args = parse_arguments()
 
-    while(True):
-        audio_file = raw_input("Write the path to an audio file (at least 2 min duration)")
-
-        x, Fs = librosa.load(audio_file)
+    if args.task == "featureExtractionDataset":
+        extractFeaturesFolder(args.output_DataFrame, args.dataset_folder, args.mtwin, args.mtstep, args.stwin, args.ststep)
+    elif args.task == "bestForestClassifier":
+        df = pd.DataFrame.from_csv(args.DataFrame)
+        opt = ForestOptimizer(df)
+        clf = opt.optimizeClf(args.population, args.generations)
+        clf = KFoldCrossValidation(df, args.report_folder, clf)
+        joblib.dump(clf, args.clf_file)
+    elif args.task == "bestTreeClassifier":
+        df = pd.DataFrame.from_csv(args.DataFrame)
+        opt = TreeOptimizer(df)
+        clf = opt.optimizeClf(args.population, args.generations)
+        clf = TreeKFoldReport(df, args.report_folder, clf)
+        joblib.dump(clf, args.clf_file)
+    elif args.task == "predictClass":
+        clf = joblib.load(args.classifier)
+        x, Fs = librosa.load(args.input)
         x = librosa.resample(x, Fs, 22050)
         x = librosa.to_mono(x)
-        feats = classifier.extractFeatures(22050, x[:22050*120], 1, 1, 0.05, 0.05)
+        feats = extractFeatures(22050, x[:22050 * 120], 1, 1, 0.05, 0.05)
         print(clf.predict([feats]))
 
 def parse_arguments():
@@ -49,24 +58,10 @@ def parse_arguments():
     bestTreeClf.add_argument("-p", "--population", type=int, default=30, help="Initial population for genetic algorithm")
     bestTreeClf.add_argument("-g", "--generations", type=int, default=50, help="Number of generations")
 
-
+    predictClass = tasks.add_parser("predictClass", help="Predict the class of the given audio file or address")
+    predictClass.add_argument("-i", "--input", required=True, help="Audio filename")
+    predictClass.add_argument("-clf", "--classifier", required=True, help="Classifier filename")
 
     return parser.parse_args()
 
-if __name__ == "__main__":
-    args = parse_arguments()
-
-    if args.task == "featureExtractionDataset":
-        extractFeaturesFolder(args.output_DataFrame, args.dataset_folder, args.mtwin, args.mtstep, args.stwin, args.ststep)
-    elif args.task == "bestForestClassifier":
-        df = pd.DataFrame.from_csv(args.DataFrame)
-        opt = ForestOptimizer(df)
-        clf = opt.optimizeClf(args.population, args.generations)
-        clf = KFoldCrossValidation(df, args.report_folder, clf)
-        joblib.dump(clf, args.clf_file)
-    elif args.task == "bestTreeClassifier":
-        df = pd.DataFrame.from_csv(args.DataFrame)
-        opt = TreeOptimizer(df)
-        clf = opt.optimizeClf(args.population, args.generations)
-        clf = TreeKFoldReport(df, args.report_folder, clf)
-        joblib.dump(clf, args.clf_file)
+if __name__ == "__main__": main()
